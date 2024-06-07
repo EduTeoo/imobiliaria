edab4323430e640309e0094a738006f4b89084d6
Eduardo Teodoro Resende Lima <23435@etehort.ps.sp>
2024-06-06 22:19:28 -0300

    imobiliaria

--- index.php
+++ index.php
@@ -10,13 +10,13 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Cadastro de Usuário e Imóvel</title>
+    <title>imovel e usuario cadastro</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
 <body>
     <nav>
-        <a href="index.php?page=usuario&action=listar">Gerenciar Usuários</a>
-        <a href="index.php?page=imovel&action=listar">Gerenciar Imóveis</a>
+        <a href="index.php?page=usuario&action=listar">Usuários</a>
+        <a href="index.php?page=imovel&action=listar">=Imóveis</a>
     </nav>
     <?php
         if (isset($_GET['page'])) {
